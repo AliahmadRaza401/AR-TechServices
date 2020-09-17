206f936efdf0c3e55293c48e79364d4249ffd0d5
ali raza <aliahmadraza401@gmail.com>
2020-09-18 00:07:00 +0500

    Updates

---
+++ precache-manifest.0f41ca8e3648b862a2bdd9b0d1f199c2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dade0e6477da687a78627e68d1aad9be",
+    "url": "/AR-TechServices/index.html"
+  },
+  {
+    "revision": "63d15e68030a4d761145",
+    "url": "/AR-TechServices/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "8b43659f766fbe9de893",
+    "url": "/AR-TechServices/static/css/main.4d3a6df5.chunk.css"
+  },
+  {
+    "revision": "63d15e68030a4d761145",
+    "url": "/AR-TechServices/static/js/2.b683005c.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/AR-TechServices/static/js/2.b683005c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b43659f766fbe9de893",
+    "url": "/AR-TechServices/static/js/main.8a4d4048.chunk.js"
+  },
+  {
+    "revision": "994f6cb781bd0480a623",
+    "url": "/AR-TechServices/static/js/runtime-main.45f3d69b.js"
+  },
+  {
+    "revision": "0d693b81feec90d9361a24dd6b785c38",
+    "url": "/AR-TechServices/static/media/Copy.0d693b81.png"
+  },
+  {
+    "revision": "8b2f6be586197a3ce605e17b361ad30e",
+    "url": "/AR-TechServices/static/media/about.8b2f6be5.png"
+  },
+  {
+    "revision": "292ce182b130a6f0dcbfa2b29651b23e",
+    "url": "/AR-TechServices/static/media/app.292ce182.jpg"
+  },
+  {
+    "revision": "0544053eb5261a834079172e5f7c6b4f",
+    "url": "/AR-TechServices/static/media/dev.0544053e.jpg"
+  },
+  {
+    "revision": "37993bccaf12f4add6d1f56e4fd74253",
+    "url": "/AR-TechServices/static/media/gm.37993bcc.jpg"
+  },
+  {
+    "revision": "a399f6b8944bf92e672a4a7e082b918c",
+    "url": "/AR-TechServices/static/media/grp.a399f6b8.jpg"
+  },
+  {
+    "revision": "a6e34201cec515357557444c44d6ebfe",
+    "url": "/AR-TechServices/static/media/h1.a6e34201.png"
+  },
+  {
+    "revision": "c958cebb7571136bcae11c1a8756b53d",
+    "url": "/AR-TechServices/static/media/so.c958cebb.jpg"
+  }
+]);
